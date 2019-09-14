536001fabbbfd6082324328e6374a8cc78b2e0d0
cristian david tilano silva <cristiantilanosilva@gmail.com>
2019-09-13 20:55:04 -0500

    punto 4 del taller 5

---
+++ taller5.5/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace taller5._5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblSegundos = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnContar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblSegundos
+            // 
+            this.lblSegundos.AutoSize = true;
+            this.lblSegundos.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSegundos.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSegundos.Location = new System.Drawing.Point(125, 59);
+            this.lblSegundos.Name = "lblSegundos";
+            this.lblSegundos.Size = new System.Drawing.Size(57, 41);
+            this.lblSegundos.TabIndex = 0;
+            this.lblSegundos.Text = "10";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnContar
+            // 
+            this.btnContar.Location = new System.Drawing.Point(107, 153);
+            this.btnContar.Name = "btnContar";
+            this.btnContar.Size = new System.Drawing.Size(75, 23);
+            this.btnContar.TabIndex = 1;
+            this.btnContar.Text = "Contar";
+            this.btnContar.UseVisualStyleBackColor = true;
+            this.btnContar.Click += new System.EventHandler(this.btnContar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnContar);
+            this.Controls.Add(this.lblSegundos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSegundos;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnContar;
+    }
+}
+
---
+++ taller5.5/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace taller5._5
+{
+    public partial class Form1 : Form
+    {
+
+        int x = 10;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblSegundos.Text = x.ToString();
+            x--;
+
+            if (x < 0) {
+                timer1.Stop();
+            }
+        }
+
+        private void btnContar_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+    }
+}
